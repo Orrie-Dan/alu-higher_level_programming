5fde9b731d753c76502f3311115dbadcbd1d8ed2
Dan <d.nkusi@alustudent.com>
2024-10-24 06:16:15 -0700

    commit 7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def uppercase (str):
-    for iterator in str:
-        temp = iterator
-    if ord (temp) >= 97 and ord(temp) <=122:
-        temp = chr(ord(iterator) -32)
-        print("{}".format{temp}, end='')
-        print ()
+
+print("a is {}".format("lower" if islower("a") else "upper"))
+print("H is {}".format("lower" if islower("H") else "upper"))
+print("A is {}".format("lower" if islower("A") else "upper"))
+print("3 is {}".format("lower" if islower("3") else "upper"))
+print("g is {}".format("lower" if islower("g") else "upper"))
+
