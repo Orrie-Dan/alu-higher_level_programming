4ee689d1adc1230345ce60c11e1d26afa0f4fab4
Dan <d.nkusi@alustudent.com>
2024-10-24 08:16:09 -0700

    commit 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,13 +4,13 @@ import sys
 
 if __name__ == "__main__":
     num_arguments = len(sys.argv) - 1  # Exclude the script name
-    argument_word = "argument" if num_arguments == 1 else "arguments"
-    
-    # Print the number of arguments
+
     if num_arguments == 0:
         print("Number of arguments: 0.")
     else:
-        print("Number of argument(s): {} {}:".format(num_arguments, argument_word))
+        argument_word = "argument" if num_arguments == 1 else "arguments"
+        print("{} {}:".format(num_arguments, argument_word))
+        
         # Print each argument
         for i in range(1, len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))
