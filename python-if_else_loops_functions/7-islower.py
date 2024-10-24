f90fd420fef732ad940ce6786393695d6af08605
Dan <d.nkusi@alustudent.com>
2024-10-24 07:20:34 -0700

    commit 7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-islower = __import__('7-islower').islower
+def islower(c):
+    return ord(c) >= ord('a') and ord(c) <= ord('z')
 
-print("a is {}".format("lower" if islower("a") else "upper"))
-print("H is {}".format("lower" if islower("H") else "upper"))
-print("A is {}".format("lower" if islower("A") else "upper"))
-print("3 is {}".format("lower" if islower("3") else "upper"))
-print("g is {}".format("lower" if islower("g") else "upper"))
+# Test cases
+test_cases = ['a', 'z', 'A', 'Z', '4', '!']
+
+for char in test_cases:
+    result = islower(char)
+    print(f"Character: '{char}' - islower: {result}")
 
