02f221aeeaf4801d46e25e033898e7786efefea6
Dan <d.nkusi@alustudent.com>
2024-10-24 06:12:57 -0700

    commit 7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-def islower (c):
-    if ord (c) >= 97 and ord(c) <=122:
-        return true
-    else:
-        return false
+def uppercase (str):
+    for iterator in str:
+        temp = iterator
+    if ord (temp) >= 97 and ord(temp) <=122:
+        temp = chr(ord(iterator) -32)
+        print("{}".format{temp}, end='')
+        print ()
