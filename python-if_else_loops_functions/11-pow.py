df55a3773ee358b6dd28d62ad2c61062e8e3eecb
Dan <d.nkusi@alustudent.com>
2024-10-24 07:02:55 -0700

    commit 11

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def pow(a, b):
+    if b < 0:
+        return 1 / pow(a, -b)  # Handle negative exponent
     result = 1
     for _ in range(b):
         result *= a
     return result
+
 
