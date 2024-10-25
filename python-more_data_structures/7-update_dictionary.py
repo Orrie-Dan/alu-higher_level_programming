fba58fe45a38f55365b5a4e3c51c89d4c8992a71
Dan <d.nkusi@alustudent.com>
2024-10-25 12:58:32 -0700

    commit 7

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    if a_dictionary is None:
-        return  # Handle the case where the input is None
-
-    # Sort the keys of the dictionary and print key-value pairs
-    for key in sorted(a_dictionary.keys()):
-        print(f"{key}: {a_dictionary[key]}")
+def update_dictionary(a_dictionary, key, value):
+    # Update the dictionary with the given key and value
+    a_dictionary[key] = value
 
 # Example usage
 if __name__ == "__main__":
-    new_dict = {
-        "b": "b",
-        "a": "A",
-        "c": "c",
-        "e": "e",
-        "d": "d",
-    }
+    my_dict = {'a': 'A', 'b': 'b', 'c': 'c'}
     
-    print_sorted_dictionary(new_dict)
-    print()  # Adding a newline for clarity
-
-    # Call again to show it works multiple times
-    print_sorted_dictionary(new_dict)
+    # Update an existing key
+    update_dictionary(my_dict, 'a', 'Alpha')
+    print_sorted_dictionary(my_dict)  # After updating 'a'
+    
+    # Add a new key
+    update_dictionary(my_dict, 'd', 'd')
+    print_sorted_dictionary(my_dict)  # After adding 'd'
 
