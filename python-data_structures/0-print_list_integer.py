64022f2c1a6abe95be7bb506ca140181d8df7868
Dan <d.nkusi@alustudent.com>
2024-10-25 06:58:23 -0700

    commit 1

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for integer in my_list:
-        print("{}".format(integer))
+    for item in my_list:
+        if isinstance(item, int):
+            print("{}".format(item))
+        else:
+            raise TypeError("List must only contain integers")
 
