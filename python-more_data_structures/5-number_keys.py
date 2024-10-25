cdc729dd98524efb38778e8132757c0c839965b5
Dan <d.nkusi@alustudent.com>
2024-10-25 12:44:16 -0700

    commit 5

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-my_dict = {'name': 'John', 'age': 30, 'city': 'New York'}
-result = number_keys(my_dict)
-print(result)  # Output: 3
+def number_keys(a_dictionary):
+    return len(a_dictionary)
+
+# Example usage:
+if __name__ == "__main__":
+    my_dict = {'name': 'John', 'age': 30, 'city': 'New York'}
+    result = number_keys(my_dict)
+    print(result)  # Output: 3
 
