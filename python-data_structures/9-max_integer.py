0867ad8e7b875a3b9f00f9f900e60d313e3ac7d6
Dan <d.nkusi@alustudent.com>
2024-10-25 09:37:04 -0700

    commit 8

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-def multiple_returns(sentence):
-    if len(sentence) == 0:
-        return (0, None)  # If the sentence is empty
-    else:
-        return (len(sentence), sentence[0])  # Length and first character
+def max_integer(my_list=[]):
+    if not my_list:
+        return None  # Return None if the list is empty
+    max_value = my_list[0]  # Initialize max_value with the first element
+    for num in my_list:
+        if num > max_value:
+            max_value = num  # Update max_value if a larger number is found
+    return max_value
+
 
