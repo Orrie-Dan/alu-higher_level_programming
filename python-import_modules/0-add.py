fd76b32e1937a2fcebc09146e88b5fb1dfe0721c
Dan <d.nkusi@alustudent.com>
2024-10-25 05:58:07 -0700

    commit 1

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,23 +1,12 @@
 #!/usr/bin/python3
 
+if __name__ == "__main__":
+
 from add_0 import add
 
-if __name__ == "__main__":
+
     a = 1
     b = 2
     result = add(a, b)
     print(f"{a} + {b} = {result}")
- cases = [
-        (1, 2),
-        (10, 30),
-        (-10, 30),
-        (-10, -30),
-        (5, "H")
-    ]
-
-    for a, b in cases:
-        try:
-            result = add(a, b)
-            print(f"{a} - {b} = {result}")
-        except TypeError:
-            print(f"{a} - {b} = Error: Unsupported operation")
+ 
