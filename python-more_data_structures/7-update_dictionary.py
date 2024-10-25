72ce047fa61e24325f2e2d37c51b2cae4e80a66d
Dan <d.nkusi@alustudent.com>
2024-10-25 13:34:18 -0700

    commit 7

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -5,13 +5,13 @@ def update_dictionary(a_dictionary, key, value):
 
 # Example usage
 if __name__ == "__main__":
-    my_dict = {'a': 'A', 'b': 'b', 'c': 'c'}
-    
+    my_dict = {'a': 1, 'b': 2, 'c': 3}
+
     # Update an existing key
-    update_dictionary(my_dict, 'a', 'Alpha')
-    print_sorted_dictionary(my_dict)  # After updating 'a'
-    
+    update_dictionary(my_dict, 'b', 5)
+    print(my_dict)  # Output: {'a': 1, 'b': 5, 'c': 3}
+
     # Add a new key
-    update_dictionary(my_dict, 'd', 'd')
-    print_sorted_dictionary(my_dict)  # After adding 'd'
+    update_dictionary(my_dict, 'd', 4)
+    print(my_dict)  # Output: {'a': 1, 'b': 5, 'c': 3, 'd': 4}
 
