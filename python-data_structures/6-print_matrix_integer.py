66d2e211ebcbe628d0627892e8810f648fbfdf5b
Dan <d.nkusi@alustudent.com>
2024-10-25 09:03:56 -0700

    commit 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,8 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in range(len(row)):
             if i == len(row) - 1:
-                print("{}".format(row[i]), end='')  # Print without trailing space
+                print("{:d}".format(row[i]), end='')  # No trailing space
             else:
-                print("{}".format(row[i]), end=' ')
+                print("{:d}".format(row[i]), end=' ')
         print()  # Move to the next line after each row
 
