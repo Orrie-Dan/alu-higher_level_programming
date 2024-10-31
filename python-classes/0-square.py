6557869f6911ff505d5a6ec2b8c8c73568fe12f2
Dan <d.nkusi@alustudent.com>
2024-10-31 05:41:41 -0700

    commit 1

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 class Square:
-    pass
+    """Represents a square."""
+
+    def __init__(self):
+        """Initialize a new Square instance."""
+        self.dict_ = {}
+
+# Usage example
+mysquare = Square()
+print(type(mysquare))  # Output: <class '__main__.Square'>
+print(mysquare.dict_)  # Output: {}
