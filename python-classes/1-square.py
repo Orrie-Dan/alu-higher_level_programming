4359aefa8b86698d8289b604078d1c2f91bf7879
Dan <d.nkusi@alustudent.com>
2024-11-05 06:12:43 -0800

    commit 2

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,33 +1,18 @@
 #!/usr/bin/python3
 """
-Module for representing a square.
+Module that defines a Square class with a private attribute 'size'.
 """
 
 class Square:
-    """Represents a square with a private size attribute."""
+    """
+    A class that defines a square by its size.
+    The size attribute is private.
+    """
 
     def __init__(self, size):
-        """Initialize a new Square instance with a given size."""
+        """
+        Initializes a new instance of the Square class with a given size.
+        
+        The size attribute is private (denoted by the __ prefix).
+        """
         self.__size = size  # Private instance attribute
-        self.dict_ = {}     # Public attribute
-
-# Usage example
-if __name__ == "__main__":
-    mysquare = Square(3)
-    print(type(mysquare))  # Expected output: <class '__main__.Square'>
-    print(mysquare.dict_)   # Expected output: {}
-
-    mysquare = Square(89)
-    print(type(mysquare))  # Expected output: <class '__main__.Square'>
-    print(mysquare.dict_)   # Expected output: {}
-
-    # Testing access to private attribute
-    try:
-        print(mysquare.__size)
-    except Exception as e:
-        print(e)  # Expected output: AttributeError
-
-    try:
-        print(mysquare._Square__size)
-    except Exception as e:
-        print(e)  # Expected output: AttributeError
