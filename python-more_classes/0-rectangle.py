f8b3205a96c931c8aacb88a95a3efb0b6fe72460
Dan <d.nkusi@alustudent.com>
2024-11-06 06:49:47 -0800

    commit 1

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
+ """
+    An empty class representing a rectangle.
+
+    This class currently has no attributes or methods.
+    It serves as a placeholder for a rectangle that could be expanded
+    in the future with attributes like width, height, etc., and methods
+    such as calculating the area or perimeter.
+    """
 class Rectangle:
-    """Empty class defining a rectangle."""
+    """
+    An empty class representing a rectangle.
+
+    This class currently has no attributes or methods.
+    It serves as a placeholder for a rectangle that could be expanded
+    in the future with attributes like width, height, etc., and methods
+    such as calculating the area or perimeter.
+    """
     pass
 
