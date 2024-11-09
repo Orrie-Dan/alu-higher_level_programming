78acb2312884b7204615e2bf7519f8aa8d0d0059
Dan <d.nkusi@alustudent.com>
2024-11-09 07:20:18 -0800

    commit2

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module that defines the BaseGeometry class.
+This class is an empty base class for geometry-related classes.
+"""
+
+class BaseGeometry:
+    """An empty class representing basic geometry."""
+    pass
+
