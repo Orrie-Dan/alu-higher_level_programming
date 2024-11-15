2f496ae487b39f0fbedfdfec408704958a72c588
Dan <d.nkusi@alustudent.com>
2024-11-15 07:10:32 -0800

    commit 5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,5 @@
--- This script shows the columns (full description) of the first_table in the current database
-SHOW COLUMNS FROM first_table;
+-- This script retrieves the full description of the first_table from the database
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
 
