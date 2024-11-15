8b87ad7452da49f9c44e1b8a64c152f36b2ef38a
Dan <d.nkusi@alustudent.com>
2024-11-15 14:32:54 -0800

    commit 1

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,6 +1,6 @@
 -- List specific privileges for user_0d_1 on user_2_db
 SHOW GRANTS FOR 'user_0d_1'@'localhost' 
-  LIKE '%user_2_db%';
+  LIKE '%user_1_db%';
 
 -- List specific privileges for user_0d_2 on user_2_db
 SHOW GRANTS FOR 'user_0d_2'@'localhost' 
