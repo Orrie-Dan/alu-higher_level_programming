a42ce4a43ef27b0a34b7f842523094920ccf44f2
Dan <d.nkusi@alustudent.com>
2024-11-16 12:26:20 -0800

    commit

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,7 @@
--- List all shows, even those without a genre, sorted by title and genre_id
+-- List all shows that have at least one genre linked, sorted by title and genre_id
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NOT NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
 
