9435a17e8e7c10479bfd7b364210a8eaa2c93920
Dan <d.nkusi@alustudent.com>
2024-11-16 12:00:07 -0800

    commit8

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 --  script that lists all cities contained in the database
-USE hbtn_0d_usa;
+
 SELECT cities.id, cities.name AS city_name, states.name AS state_name
 FROM cities
 JOIN states ON cities.state_id = states.id
