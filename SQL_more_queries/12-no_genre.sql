8ba55e3e6027edb48b03ef938a2e725b57392b15
Dan <d.nkusi@alustudent.com>
2024-11-16 09:02:20 -0800

    commit 10

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,10 @@
+-- Use the provided database (replace 'your_database_name' with the actual database)
+USE hbtn_0d_tvshows;
+
+-- Select all shows without a linked genre
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+
