1490aa9219501293ea6b5795c595505b9bf01498
Dan <d.nkusi@alustudent.com>
2024-11-16 08:49:30 -0800

    commit 10

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,9 @@
+-- Use the provided database 
+USE hbtn_0d_tvshows;
+
+-- List all shows and their genres (including NULL for shows with no genre)
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+
