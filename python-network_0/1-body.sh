cf5b84bb4fc583552cf41a0851ca5094853c0b90
Dan <d.nkusi@alustudent.com>
2024-11-20 08:37:30 -0800

    commit 2

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send GET request and handle redirections
-redirects=$(curl -s -L -w "%{num_redirects}" -o /dev/null "$1"); [ "$redirects" -eq 0 ] && echo "Direct access" || { [ "$redirects" -eq 1 ] && echo "With one redirection" || echo "With $redirects redirections"; } 
+redirects=$(curl -s -L -w "%{num_redirects}" -o /dev/null "$1"); [ "$redirects" -eq 0 ] && echo "Direct access"|| { [ "$redirects" -eq 1 ] && echo "With one redirection"|| echo "With $redirects redirections";} 
