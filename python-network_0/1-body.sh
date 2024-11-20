ec7e8faa590308abe4e17d8e00e12e78958956ac
Dan <d.nkusi@alustudent.com>
2024-11-20 08:27:19 -0800

    commit 2

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send GET request, follow redirections, and count how many redirects occurred
-curl -s -L -w "%{num_redirects}" -o /dev/null "$1" 
+redirects=$(curl -s -L -w "%{num_redirects}" -o /dev/null "$1"); [ "$redirects" -eq 0 ] && echo "Direct access" || echo "With $redirects redirections" 
