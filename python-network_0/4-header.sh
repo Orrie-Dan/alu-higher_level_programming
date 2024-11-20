fd2f07080b644ce14665e3bf9acd3be00e19656d
Dan <d.nkusi@alustudent.com>
2024-11-20 08:50:21 -0800

    commit 4

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send GET request with custom header and display the body
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -s -H "X-HolbertonSchool-User-Id: 98" "$1" | grep -i "OK"
