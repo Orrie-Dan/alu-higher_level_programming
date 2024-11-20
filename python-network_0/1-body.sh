63deec83642832dcdbb0e0141d8d1bf01216a2e0
Dan <d.nkusi@alustudent.com>
2024-11-20 08:16:06 -0800

    commit 2

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send GET request, check for 200 status code, and display body if 200
-curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q "^200$" && curl -s "$1"
+# Send GET request, follow redirections, and count how many redirects occurred
+curl -s -L -w "%{num_redirects}" -o /dev/null "$1" 
