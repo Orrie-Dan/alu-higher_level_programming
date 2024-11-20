0187b09356c2301442928924ea223418e70c6adc
Dan <d.nkusi@alustudent.com>
2024-11-20 06:17:22 -0800

    commit 1

---
+++ python-network_0/0-body_size.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Check if a URL argument is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <URL>"
+  exit 1
+fi
+
+# Send the request and extract the size of the body
+curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
+
