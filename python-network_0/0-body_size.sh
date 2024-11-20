85541f201a573bbcb5fe599108dda8deebd3bd29
Dan <d.nkusi@alustudent.com>
2024-11-20 06:35:29 -0800

    commit 1

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Check if a URL argument is provided
-[ -z "$1" ] && echo "Usage: $0 <URL>" && exit 1 curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
+# Check if the URL is provided, else show usage message and exit
+[ -z "$1" ] && echo "Usage: $0 <URL>" && exit 1
+curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
