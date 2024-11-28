9d267984b98af5d0112cd5ca79f70b2783ae3cc1
Dan <d.nkusi@alustudent.com>
2024-11-28 08:56:57 -0800

    commit 1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -2,16 +2,27 @@
 """A script that takes in a URL,
 Sends a request to the URL,
 And displays the value of the X-Request-Id
-variable found in the header ofthe response.
+variable found in the header of the response.
 """
 
 import sys
 import urllib.request
 
-
 if __name__ == "__main__":
+    # Get URL from the command line argument
     url = sys.argv[1]
 
+    # Create the request object
     request = urllib.request.Request(url)
+
+    # Open the URL and follow redirections automatically
     with urllib.request.urlopen(request) as resp:
-        print(dict(resp.headers).get("X-Request-Id"))
+        # Retrieve the X-Request-Id from the response headers, if it exists
+        x_request_id = resp.headers.get("X-Request-Id")
+
+        # If the X-Request-Id header is not found, print None (or handle it differently)
+        if x_request_id:
+            print(x_request_id)
+        else:
+            print("X-Request-Id not found")
+
